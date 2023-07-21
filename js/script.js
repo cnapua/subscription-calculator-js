78f44189099a30343a75c974ae59a2fb7b637da7
caitlin_d <caitlin.duquette@gmail.com>
2023-07-20 18:46:12 -1000

    JS 1 and 2

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    // console.log(subType);
+    updateSubscriptionDiv();
+}); 
+
+subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value);
+    // console.log(subDuration);
+    updateSubscriptionDiv();
+});
